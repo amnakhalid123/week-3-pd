e754d6bbda9912c0edbd1d28e2005a5635ebcdda
amnakhalid123 <144409342+amnakhalid123@users.noreply.github.com>
2023-09-24 15:47:15 +0500

    Add files via upload

---
+++ task13.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+main()
+{
+       int sum=0;
+       int num;
+       cout << "Enter 1st number";
+       cin >> num;
+       sum=sum+num;
+       cout << "Enter 2nd number";
+       cin >> num;
+       sum=sum+num;
+       cout << "Enter 3rd number";
+       cin >> num;
+       sum=sum+num;
+       cout << "Enter 4th number";
+       cin >> num;
+       sum=sum+num;
+       cout << "Enter 5th number";
+       cin >> num;
+       sum=sum+num;
+       cout << "sum"<<sum;
+          return 0;
+
+}
